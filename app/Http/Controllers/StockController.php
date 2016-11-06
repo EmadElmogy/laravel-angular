635f53551c3ea9dd2de70ee1d98205be93c6b2eb
root <root@192.168.1.2>
2016-11-06 16:12:04 +0200

    fix stock report

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -71,7 +71,7 @@ class StockController extends BaseController
               return $q->where('variations.product_id', request('filters.product_id'));
           })
           ->join('variations_stock','variations.id','=','variations_stock.variation_id')
-          ->where('variations_stock.door_id','=',$item_id)
+        //  ->where('variations_stock.door_id','=',$item_id)
           ->select('product_id','variations.name','barcode','stock')->get();
 
       $stock = collect(DB::table('variations_stock')->get());
