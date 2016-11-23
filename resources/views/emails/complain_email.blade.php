452ba6e2a11e38532e0af079309add7fcfd33da6
root <root@192.168.1.2>
2016-11-23 16:55:04 +0200

    add mail complain

--- resources/views/emails/complain_email.blade.php
+++ resources/views/emails/complain_email.blade.php
@@ -2,6 +2,7 @@
 
 Type : {{$item->type}} <br>
 comment : {{$item->comment}} <br>
+Advisor : {{$item->advisor->name}} <br>
+Door : {{$item->door->name}} <br>
 date : {{$item->date}} <br>
-{{url($item->image)}}
 <img src="{{url($item->image)}}" style="padding:0px; margin:0px" />
