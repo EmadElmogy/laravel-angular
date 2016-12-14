5e4e698da56b4c692acbdebf9d1270bb9fdbd21c
root <root@192.168.1.2>
2016-12-14 18:24:28 +0200

    time range edits

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -195,7 +195,7 @@ class ReportsController extends BaseController
             ->join('doors', 'doors.id', '=', 'reports.door_id')
             ->groupBy('reports.door_id')
             ->select('doors.name as door_name')
-            ->selectRaw('SUM(sales) as sales , SUM(basket_value) as sell_out')
+            ->selectRaw('SUM(sales) as sales , SUM(basket_value) as basket_value')
             ->orderBy('sales', 'DESC')
             ->when(request('from_date') && ! request('to_date'), function ($q) {
                 return $q->whereDate('reports.date', '=', request('from_date'));
@@ -204,12 +204,32 @@ class ReportsController extends BaseController
                 return $q->whereBetween('reports.date', [request('from_date'), request('to_date')]);
             })
             ->get();
-        Excel::create('Door_reports', function($excel) use($results) {
-            $excel->sheet('Sheet 1', function($sheet) use($results) {
+
+            $avg_results = DB::table('report_products')
+                ->join('reports', 'reports.id', '=', 'report_products.report_id')
+                ->join('doors', 'doors.id', '=', 'reports.door_id')
+                ->groupBy('reports.door_id')
+                ->select('doors.name as door_name')
+                ->selectRaw('SUM(sales) as sales , SUM(basket_value) as basket_value')
+                ->orderBy('sales', 'DESC')
+                ->when(request('from_date') && ! request('to_date'), function ($q) {
+                    return $q->whereDate('reports.date', '=', request('from_date'));
+                })
+                ->when(request('from_date') && request('to_date'), function ($q) {
+                    return $q->whereBetween('reports.date', [request('from_date'), request('to_date')]);
+                })
+                ->avg('sales');
+        Excel::create('Door_reports', function($excel) use($results,$avg_results) {
+            $excel->sheet('Sheet 1', function($sheet) use($results,$avg_results) {
+                // $sum_sales=0;
                 foreach ($results as &$result) {
                     $result = (array)$result;
+                    // $sum_sales += $result->sales;
                 }
                 $sheet->fromArray($results);
+                $sheet->appendRow(array(
+                    '', '','Average Basket Size',$avg_results
+                ));
             });
         })->export('xls');
     }
