1ee239adf04a4866e320477efad8fd7aa1b90ae5
emad <emadelmogy619@gmail.com>
2017-01-11 12:52:09 +0200

    fix basekt value

--- resources/views/reports/by-advisors.blade.php
+++ resources/views/reports/by-advisors.blade.php
@@ -68,7 +68,7 @@
                                     @else
                                     <td>HairCare</td>
                                     @endif
-                                    <td>{{$basket_value_x}}</td>
+                                    <td>{{$item->advisor_id}}</td>
                                 </tr>
                             @endforeach
                             </tbody>
