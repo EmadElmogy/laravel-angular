f770a2bc01deea9629f313af08df8f794d135876
emad <emadelmogy619@gmail.com>
2017-01-12 11:38:31 +0200

    fix basekt value

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -191,11 +191,13 @@ class ReportsController extends BaseController
     }
     public function excelbyDoor(){
         $results = DB::table('report_products')
+            ->join('variations', 'variations.id', '=', 'report_products.variation_id')
+            ->join('products', 'products.id', '=', 'variations.product_id')
             ->join('reports', 'reports.id', '=', 'report_products.report_id')
             ->join('doors', 'doors.id', '=', 'reports.door_id')
             ->groupBy('reports.door_id')
             ->select('doors.name as door_name','doors.id as door_id')
-            ->selectRaw('SUM(sales) as sales , SUM(basket_value) as basket_value , COUNT(`reports`.`customer_id`) AS Customers , AVG(sales) as average_basket_size ')
+            ->selectRaw('SUM(sales) as sales , SUM(report_products.sales*products.price) as basket_value , COUNT(`reports`.`customer_id`) AS Customers , AVG(sales) as average_basket_size ')
             ->orderBy('sales', 'DESC')
             ->when(request('from_date') && ! request('to_date'), function ($q) {
                 return $q->whereDate('reports.date', '=', request('from_date'));
